543f5eba530922020de096939e4c73f23788a262
HCEspino <harrison.espino@gmail.com>
2023-10-12 20:16:02 -0700

    learning a little bit

--- generativeNN/models.py
+++ generativeNN/models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import math
 
 class TransitionModel(nn.Module):
     def __init__(self, input_size, hidden_size, output_size=8):
@@ -66,44 +67,38 @@ class LikelihoodModel(nn.Module):
         x = self.layer3(x)
         return x
 
-def sample(mean, log_variance):
-    # Sample ε from a standard normal distribution
+def sample(mean, log_std):
+    std_dev = torch.exp(log_std)
     epsilon = torch.randn_like(mean)
-    
-    # Reparameterization: s = μ + ε * σ, where σ = exp(0.5 * log_variance)
-    std_deviation = torch.exp(0.5 * log_variance)
-    sampled_value = mean + epsilon * std_deviation
-    
-    return sampled_value
+    return mean + epsilon * std_dev
 
-def kl_divergence_gaussian(mu_p, log_var_p, mu_q, log_var_q):
-    var_p = torch.exp(log_var_p) ^ 2
-    var_q = torch.exp(log_var_q)
 
-    k = mu_p.size(0)  # Dimensionality of the distribution
+def kl_divergence_gaussian(p_mean, p_logstd, q_mean, q_logstd):
+    p_std = torch.exp(p_logstd)
+    q_std = torch.exp(q_logstd)
 
-    term1 = torch.sum(var_q / var_p)
-    term2 = torch.sum((mu_q - mu_p).pow(2) / var_p)
-    term3 = k
-    term4 = torch.sum(log_var_q - log_var_p)
+    squared_term = (p_std**2 / q_std**2) 
+    log_term = q_logstd - p_logstd
 
-    kl = 0.5 * (term1 + term2 - term3 + term4)
+    kl_divergence = 0.5 * (squared_term + log_term - 1 + (q_mean - p_mean)**2 / (q_std**2))
+
+    kl = torch.sum(kl_divergence, dim=1)
+
     return kl
 
-def log_likelihood(observation, mean, log_variance):
-    variance = torch.exp(log_variance)  # Convert log variance to variance
-    log_lik = -0.5 * (torch.log(2 * torch.tensor([torch.pi], dtype=variance.dtype) * variance) + (observation - mean).pow(2) / variance)
-    return log_lik
+def log_likelihood(obs, mean, log_std):
+    std = torch.exp(log_std)
+    log_likelihood = -0.5 * ((obs - mean) / std)**2 - 0.5 * log_std - 0.5 * math.log(2 * math.pi)
+    return log_likelihood
 
-def freeEnergy(p_phi_mean, p_phi_logvar, p_theta_mean, p_theta_logvar, p_eps_mean, p_eps_logvar, obs):
+def freeEnergy(theta_mean, theta_logstd, phi_mean, phi_logstd, eps_mean, eps_logstd, obs):
     '''
     Calculate the free energy
-    p_phi_mean, p_phi_var: the mean and variance of the prior distribution over the parameters of the transition model
-    p_theta_mean, p_theta_var: the mean and variance of the prior distribution over the parameters of the posterior model
-    p_eps_mean, p_eps_var: the mean and variance of the prior distribution over the parameters of the likelihood model
+        p_phi_mean, p_phi_var: the mean and variance of the prior distribution over the parameters of the transition model
+        p_theta_mean, p_theta_var: the mean and variance of the prior distribution over the parameters of the posterior model
+        p_eps_mean, p_eps_var: the mean and variance of the prior distribution over the parameters of the likelihood model
     '''
-    kl_term = kl_divergence_gaussian(p_phi_mean, p_phi_logvar, p_theta_mean, p_theta_logvar)
-    log_likelihood_term = log_likelihood(obs, p_eps_mean, p_eps_logvar)
-
+    kl_term = kl_divergence_gaussian(phi_mean, phi_logstd, theta_mean, theta_logstd)
+    log_likelihood_term = log_likelihood(obs, eps_mean, eps_logstd)
     return kl_term - log_likelihood_term
     
--- generativeNN/train.py
+++ generativeNN/train.py
@@ -6,6 +6,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from models import TransitionModel, PosteriorModel, LikelihoodModel, freeEnergy, sample
 
+def randomAgent(last_action):
+   #10 percent chance of turning left or right
+    if np.random.rand() < 0.1:
+        #Pick 0 or 2
+        return int(np.random.choice([0, 2]))
+    else:
+        return last_action 
+
 #State space is of size 4
 #Observation space is only a single number (position)
 #Action space is turn left, right, or do nothing
@@ -17,20 +25,22 @@ action_space = 3
 transition_model = TransitionModel(state_space + action_space, 20, 8)
 posterior_model = PosteriorModel(state_space + action_space + observation_space, 20, 8)
 likelihood_model = LikelihoodModel(state_space, 20, 2)
-optimizer = optim.Adam(list(transition_model.parameters()) + list(posterior_model.parameters()) + list(likelihood_model.parameters()), lr=0.01)
+optimizer = optim.Adam(list(transition_model.parameters()) + list(posterior_model.parameters()) + list(likelihood_model.parameters()), lr=0.001)
 
 #Collect data from random policy
 print("Collecting data...")
 env = gym.make('MountainCar-v0')
 observation, info = env.reset()
-eps = 320
+eps = 1280
 data = []
+last_action = 1
 for i in range(eps):
     ep = []
     for _ in range(100):
-        action = env.action_space.sample()
-        observation, reward, terminated, truncated, info = env.step(action)
+        action = randomAgent(last_action)
+        last_action = action
 
+        observation, reward, terminated, truncated, info = env.step(action)
         one_hot_action = F.one_hot(torch.tensor(action), num_classes=3).view(1, 3)
         observation = torch.tensor(observation).view(1, 2)
         ep.append((one_hot_action, observation))
@@ -48,7 +58,7 @@ posterior_model.train()
 likelihood_model.train()
 
 batch_size = 32
-epochs = 1
+epochs = 10
 
 #Train the models
 for j in range(epochs):
@@ -63,16 +73,26 @@ for j in range(epochs):
         state = torch.zeros(1, state_space)
         loss = torch.Tensor(1, 1).fill_(0.0)
 
+        #Pairs are (at-1, ot)
         for action, obs in data[i]:
             obs = obs[:, 0].unsqueeze(1)
+
+            #Get Prior P(st | st-1, at-1)
             transition_output = transition_model(torch.cat([state, action], dim=1))
+            #and posterior Q(st | st-1, at-1, ot)
             posterior_output = posterior_model(torch.cat([state, action, obs], dim=1))
-            likelihood_output = likelihood_model(state)
 
+            #Sample prior and use to get likelihood of observation
+            state_sample = sample(posterior_output[:, :4], posterior_output[:, 4:])
+            likelihood_output = likelihood_model(state_sample)
+
+            #Sample from likelihood to get observation sample
             obs_sample = sample(likelihood_output[:, 0], likelihood_output[:, 1])
-            state_sample = sample(posterior_output[:, :4], posterior_output[:, 4:]) * torch.randn(1, 4)
 
+            #Calculate free energy
             loss += freeEnergy(transition_output[:, :4], transition_output[:, 4:], posterior_output[:, :4], posterior_output[:, 4:], likelihood_output[:, 0], likelihood_output[:, 1], obs)
+
+            #Update state
             state = state_sample
 
         if i % batch_size == 0 and i != 0:
@@ -95,8 +115,9 @@ posterior = []
 prior = []
 state = torch.zeros(1, state_space)
 
-for _ in range(1000):
-    action = env.action_space.sample()
+for _ in range(200):
+    action = randomAgent(last_action)
+    last_action = action
     observation, reward, terminated, truncated, info = env.step(action)
 
     action = F.one_hot(torch.tensor(action), num_classes=3).view(1, 3)
@@ -106,8 +127,8 @@ for _ in range(1000):
     transition_output = transition_model(torch.cat([state, action], dim=1))
     posterior_output = posterior_model(torch.cat([state, action, obs], dim=1))
 
-    prior_sample = sample(transition_output[:, :4], transition_output[:, 4:]) * torch.randn(1, 4)
-    posterior_sample = sample(posterior_output[:, :4], posterior_output[:, 4:]) * torch.randn(1, 4)
+    prior_sample = sample(transition_output[:, :4], transition_output[:, 4:])
+    posterior_sample = sample(posterior_output[:, :4], posterior_output[:, 4:])
 
     prior_obs = likelihood_model(prior_sample)
     prior_obs = sample(prior_obs[:, 0], prior_obs[:, 1])
