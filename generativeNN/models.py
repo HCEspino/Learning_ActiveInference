f74dc532e25bfa5c02d6aaa689be6c354ce4b984
HCEspino <harrison.espino@gmail.com>
2023-10-14 13:44:05 -0700

    i think i just need to train more

--- generativeNN/models.py
+++ generativeNN/models.py
@@ -1,7 +1,4 @@
-import torch
 import torch.nn as nn
-import torch.optim as optim
-import math
 
 class TransitionModel(nn.Module):
     def __init__(self, input_size, hidden_size, output_size=8):
@@ -66,48 +63,3 @@ class LikelihoodModel(nn.Module):
         x = self.relu2(x)
         x = self.layer3(x)
         return x
-
-def sample(mean, log_std):
-    std_dev = torch.exp(log_std)
-    epsilon = torch.randn_like(mean)
-    return mean + epsilon * std_dev
-
-
-def kl_divergence_gaussian(p_mean, p_logstd, q_mean, q_logstd):
-
-    p_std = torch.exp(p_logstd)
-    q_std = torch.exp(q_logstd)
-
-    p_diag = torch.diag(p_std)
-    q_diag = torch.diag(q_std)
-
-    N = p_mean.shape[0]
-
-    p_diag_inv = torch.inverse(p_diag)
-    q_diag_inv = torch.inverse(q_diag)
-
-    diff = q_mean - p_mean
-
-
-    tf = torch.trace(q_diag_inv @ p_diag)
-    det = torch.log(torch.det(q_diag)/torch.det(p_diag))
-    quad = diff.T @ q_diag_inv @ diff
-
-    return 0.5 * (tf + det + quad - N)
-
-def log_likelihood(obs, mean, log_std):
-    std = torch.exp(log_std)
-    log_likelihood = -0.5 * torch.log(2*torch.tensor(torch.pi)) - 0.5 * torch.log(std**2) - ((1 / (2*std**2))*(obs - mean)**2)
-    return log_likelihood
-
-def freeEnergy(theta_mean, theta_logstd, phi_mean, phi_logstd, eps_mean, eps_logstd, obs):
-    '''
-    Calculate the free energy
-        p_phi_mean, p_phi_var: the mean and variance of the prior distribution over the parameters of the transition model
-        p_theta_mean, p_theta_var: the mean and variance of the prior distribution over the parameters of the posterior model
-        p_eps_mean, p_eps_var: the mean and variance of the prior distribution over the parameters of the likelihood model
-    '''
-    kl_term = kl_divergence_gaussian(phi_mean, phi_logstd, theta_mean, theta_logstd)
-    log_likelihood_term = log_likelihood(obs, eps_mean, eps_logstd)
-    return kl_term - log_likelihood_term
-    
--- generativeNN/train - Copy.py
+++ generativeNN/train - Copy.py
@@ -5,7 +5,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
-from torch.nn import GaussianNLLLoss
 from torch.distributions import MultivariateNormal, Normal
 from torch.distributions.kl import kl_divergence
 from models import TransitionModel, PosteriorModel, LikelihoodModel
@@ -39,14 +38,12 @@ transition_model = TransitionModel(state_space + action_space, 20, 8).to(device)
 posterior_model = PosteriorModel(state_space + action_space + observation_space, 20, 8).to(device)
 likelihood_model = LikelihoodModel(state_space, 20, 2).to(device)
 optimizer = optim.Adam(list(transition_model.parameters()) + list(posterior_model.parameters()) + list(likelihood_model.parameters()), lr=0.001)
-nll = GaussianNLLLoss()
-
 
 #Collect data from random policy
 print("Collecting data...")
 env = gym.make('MountainCar-v0')
 observation, info = env.reset()
-eps = 320
+eps = 640
 data = []
 last_action = 1
 for i in range(eps):
@@ -73,70 +70,58 @@ posterior_model.train()
 likelihood_model.train()
 
 batch_size = 32
-epochs = 25
+epochs = 10
 
 bk = False
 
 losses = []
-diffs = []
 
 #Train the models
 for j in range(epochs):
-    print(f"====== EPOCH {j + 1} ======")
+    print("======EPOCH {}======".format(j + 1))
 
-    # Shuffle data indices to maintain alignment of actions and observations
-    shuffled_indices = np.random.permutation(len(data))
+    #Shuffle data using numpy
+    np.random.shuffle(data)
+    optimizer.zero_grad()
+    loss = torch.Tensor(1, 1).fill_(0.0).to(device)
 
-    #Iterate over batches
-    for i in range(0, len(data), batch_size):
+    for i in range(len(data)):
 
-        batch_indices = shuffled_indices[i:i + batch_size]
-        optimizer.zero_grad()
-        batch_loss = 0.0
+        state = torch.zeros(1, state_space).to(device)
 
-        diff_total = 0.0
+        #Pairs are (at-1, ot)
+        for action, obs in data[i]:
+            obs = obs[:, 0].unsqueeze(1)
+            action = action.to(device)
+            obs = obs.to(device)
 
-        #Process each episode in the batch
-        for idx in batch_indices:
-            state = torch.zeros(1, state_space).to(device)
+            #Get Prior P(st | st-1, at-1)
+            transition_output = transition_model(torch.cat([state, action], dim=1).to(device))
+            transition_output = MultivariateNormal(transition_output[:, :state_space], torch.diag(torch.exp(transition_output[:, state_space:][0])))
 
-            #Process each 100-step episode. Pairs are (at-1, ot)
-            for action, obs in data[idx]:
-                obs = obs[:, 0].unsqueeze(1)
-                action = action.to(device)
-                obs = obs.to(device)
+            #and posterior Q(st | st-1, at-1, ot)
+            posterior_output = posterior_model(torch.cat([state, action, obs], dim=1).to(device))
+            posterior_output = MultivariateNormal(posterior_output[:, :state_space], torch.diag(torch.exp(posterior_output[:, state_space:][0])))
+            state_sample = posterior_output.sample()
 
-                #Get Prior P(st | st-1, at-1)
-                transition_output = transition_model(torch.cat([state, action], dim=1).to(device))
-                transition_output = MultivariateNormal(transition_output[:, :state_space], torch.diag(torch.exp(transition_output[:, state_space:][0])))
+            #Sample prior and use to get likelihood of observation
+            likelihood_output = likelihood_model(state_sample)
+            likelihood_output = Normal(likelihood_output[:, 0], torch.exp(likelihood_output[:, 1]))
 
-                #and posterior Q(st | st-1, at-1, ot)
-                posterior_output = posterior_model(torch.cat([state, action, obs], dim=1).to(device))
-                posterior_output = MultivariateNormal(posterior_output[:, :state_space], torch.diag(torch.exp(posterior_output[:, state_space:][0])))
-                state_sample = posterior_output.sample()
+            #Calculate free energy
+            loss += (kl_divergence(posterior_output, transition_output) - likelihood_output.log_prob(obs))
 
-                #Sample prior and use to get likelihood of observation
-                likelihood_output = likelihood_model(state_sample)
-                likelihood_output = Normal(likelihood_output[:, 0], torch.exp(likelihood_output[:, 1]))
-
-                #Calculate free energy
-                batch_loss += (kl_divergence(posterior_output, transition_output) + nll(likelihood_output.mean, obs, likelihood_output.variance))#likelihood_output.log_prob(obs))
-
-                diff = abs(obs.item() - likelihood_output.sample().item())
-                diff_total += diff
-
-                #Update state
-                state = state_sample
-
-        #Average loss over batch
-        batch_loss /= len(batch_indices)
-        losses.append(batch_loss.item())
-        print(f"EPOCH {j+1}/{epochs} | {i // batch_size}/{len(data) // batch_size} | {batch_loss.item()}")
-        batch_loss.backward()
-        optimizer.step()
+            #Update state
+            state = state_sample
 
 
-        diffs.append(diff_total / len(batch_indices))
+        if i % batch_size == 0 and i != 0:
+            #loss = loss / batch_size
+            optimizer.step()
+            print(f"EPOCH {j+1}/{epochs} | {i // batch_size}/{len(data) // batch_size} | {loss.item()}")
+            optimizer.zero_grad()
+            losses.append(loss.item())
+            loss = torch.Tensor(1, 1).fill_(0.0).to(device)
 
 print("Evaluation...")
 
@@ -192,22 +177,18 @@ torch.save(posterior_model, "posterior_model.pt")
 torch.save(likelihood_model, "likelihood_model.pt")
 
 #Plot
-plt.figure(figsize=(10, 5))
+plt.figure()
 plt.ylim(min(ground_truth) - 0.1, max(ground_truth) + 0.1)
 plt.plot(ground_truth, label="Ground Truth")
 plt.plot(posterior, label="Posterior")
 plt.plot(prior, label="Prior")
 plt.legend()
 plt.savefig("results.png")
+plt.show()
 
 #Plot losses
-plt.figure(figsize=(10, 5))
+plt.figure()
 plt.plot(losses)
 plt.title("Loss")
 plt.savefig("losses.png")
-
-#Plot differences
-plt.figure(figsize=(10, 5))
-plt.plot(diffs)
-plt.title("Likelihood sample vs observation difference")
-plt.savefig("diffs.png")
+plt.show()
